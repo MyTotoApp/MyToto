f72aede60f82c0601f8f5cfeee422f4f88940301
Unknown <d229624@edu.rocwb.nl>
2018-11-01 13:53:44 +0100

    Toto Applicatie
    - effort & quotation toegevoegd

--- oefTemplate.php
+++ oefTemplate.php
@@ -3,21 +3,23 @@ include 'header.php';
 ?>
 
 <?php
-$conn= dbconnect();
+$conn = dbconnect();
 
-$sql = "SELECT `match_date`, cl1.`club_id`, cl1.`club_name` AS home, \n"
-
-    . "		cl2.club_id, cl2.`club_name` AS away, `match_home_goals`, `match_away_goals`, cmp1.competition_name, st.status\n"
-    . "		FROM tbl_match\n"
-    . "        JOIN tbl_clubs AS cl1 ON match_home_id = cl1.club_id\n"
-    . "        JOIN tbl_clubs AS cl2 ON match_away_id = cl2.club_id\n"
-    . "        JOIN tbl_competitions AS cmp1 ON match_competition = cmp1.competition_id\n"
-    . "        JOIN tbl_status AS st ON match_status = st.status_id\n"
-    . "        ORDER BY match_id ASC";
+$sql = "SELECT `match_date`, cl1.`club_id`, cl1.`club_name` AS home,\n"
+    . "        		cl2.club_id, cl2.`club_name` AS away, `match_home_goals`, `match_away_goals`, cmp1.competition_name, st.status, ef.effort, qu.quotation\n"
+    . "        		FROM tbl_match\n"
+    . "                JOIN tbl_clubs AS cl1 ON match_home_id = cl1.club_id\n"
+    . "                JOIN tbl_clubs AS cl2 ON match_away_id = cl2.club_id\n"
+    . "                JOIN tbl_competitions AS cmp1 ON match_competition = cmp1.competition_id\n"
+    . "                JOIN tbl_status AS st ON match_status = st.status_id\n"
+    . "                JOIN tbl_effort AS ef ON match_effort = ef.effort_id\n"
+    . "                JOIN tbl_quotation AS qu ON match_quotation = qu.quotation_id\n"
+    . "                ORDER BY match_id ASC";
 
 $result = $conn->query($sql);
 
-?> 
+?>
+
 <div class="container">
   <div class="row">
     <div class="col-md-12">
@@ -30,6 +32,8 @@ $result = $conn->query($sql);
                 <th scope="col">Away</th>
                 <th scope="col">Competition</th>
                 <th scope="col">Date</th>
+                <th scope="col">Effort</th>
+                <th scope="col">Quotation</th>
                 <th scope="col">Status</th>
             </tr>
           </thead>
@@ -50,6 +54,10 @@ $result = $conn->query($sql);
                 echo "<td>";
                 echo $row["match_date"];
                 echo "<td>";
+                echo $row["effort"];
+                echo "<td>";
+                echo $row["quotation"];
+                echo "<td>";
                 echo $row["status"];
 
 
@@ -60,7 +68,10 @@ $result = $conn->query($sql);
             }
             $conn->close();
             ?>
-      </table></div>
+          
+      </table>
+
+    </div>
 
     </div>
   </div>
